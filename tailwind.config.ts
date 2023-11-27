5180c63d0d20e63bb4d2e26d3232c62fad1869f6
Vinícius Almeida <81371812+viniciusvalmeida@users.noreply.github.com>
2023-11-27 10:18:29 -0300

    feat: set up colors and font family

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,27 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				primary: {
+					"Soft red": "hsl(10, 79%, 65%)",
+					Cyan: "hsl(186, 34%, 60%)",
+				},
+				neutral: {
+					"Dark brown": "hsl(25, 47%, 15%)",
+					"Medium brown": "hsl(28, 10%, 53%)",
+					Cream: "hsl(27, 66%, 92%)",
+					"Very pale orange": "hsl(33, 100%, 98%)",
+				},
+			},
+		},
+	},
+	plugins: [],
+};
+export default config;
